b765986bdcf1434f663ce8478457323fd8b86480
Senthil Kannan <senthilkannanarmy@gmail.com>
2019-12-13 12:52:20 +0530

    Updates

---
+++ precache-manifest.49c1e65a87fe0a2db006695d417a4eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be78e4fb58ff7642ec6dd4f56d50aa5",
+    "url": "/pagination_reactjs/index.html"
+  },
+  {
+    "revision": "3160aa2ad655b1e73ba1",
+    "url": "/pagination_reactjs/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d2b759231dbeba81e696",
+    "url": "/pagination_reactjs/static/js/2.ecd56469.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/pagination_reactjs/static/js/2.ecd56469.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3160aa2ad655b1e73ba1",
+    "url": "/pagination_reactjs/static/js/main.9458b199.chunk.js"
+  },
+  {
+    "revision": "a250bf4848e469b44473",
+    "url": "/pagination_reactjs/static/js/runtime-main.0aca8695.js"
+  }
+]);
